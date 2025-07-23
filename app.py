bc9eb37cc739d43f8cacdf375432539d5bc212a5
Amirthavarshini05 <230701027@rajalakshmi.edu.in>
2025-07-23 18:00:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from rag_pipeline import get_response
 
-st.title("📄 Offline RAG Chatbot")
+st.title("Offline RAG Chatbot")
 query = st.text_input("Enter your question")
 if st.button("Ask"):
     answer = get_response(query)
